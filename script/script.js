e34153b13315b92637b24438c87b2e4674e04304
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-03-17 13:57:38 +0100

    Added other field

--- script/script.js
+++ script/script.js
@@ -1,5 +1,16 @@
 let player1;
 let player2;
+let fields = [
+    null,
+    null,
+    'circle',
+    null,
+    'cross',
+    null,
+    null,
+    null,
+    null
+];
 
 
 function goToChoseNamesForPlayers() {
@@ -41,4 +52,40 @@ function saveNames() {
 
 function initGameField(container) {
     container.innerHTML = '';
+}
+
+
+function init() {
+    render();
+}
+
+
+function render() {
+    let content = document.getElementById('content');
+    let tableHTML = '<table>';
+    for (let i = 0; i < 3; i++) {
+        tableHTML += '<tr>';
+        for (let j = 0; j < 3; j++) {
+            let index = i * 3 + j;
+            let symbol = fields[index];
+            if (symbol === 'circle') {
+                tableHTML += '<td>o</td>';
+            } else if (symbol === 'cross') {
+                tableHTML += '<td>x</td>';
+            } else {
+                tableHTML += '<td></td>';
+            }
+        }
+        tableHTML += '</tr>';
+    }
+    tableHTML += '</table>';
+    content.innerHTML = '';
+    content.innerHTML += /*html*/`
+        <span class="content-text">It's <b>Player 1</b>'s turn.</span>
+        <div class="sign-of-player">
+            <div>x</div>
+            <div>o</div>
+        </div>
+    `;
+    content.innerHTML += tableHTML;
 }
